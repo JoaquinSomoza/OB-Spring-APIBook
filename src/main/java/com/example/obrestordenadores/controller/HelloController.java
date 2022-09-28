ea362eec1dcc4483db013595ac3c99e5a72b90f5
JoaquinSomoza <joaquinsomoza6@gmail.com>
2022-09-28 12:08:45 -0300

    HelloController - Ej 1, sesiones 4, 5 y 6

---
+++ src/main/java/com/example/obrestordenadores/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.obrestordenadores.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("api/hola")
+    public String saludo(){
+        return "Hola Mundo!";
+    }
+}
+
+// Ejercicio 1. Sesiones 4, 5 y 6.
+//Crear una clase HelloController que sea un controlador REST.
+// Dentro de la clase crear un método que retorne un saludo.
+// Probar que retorna el saludo desde el navegador y desde Postman.
