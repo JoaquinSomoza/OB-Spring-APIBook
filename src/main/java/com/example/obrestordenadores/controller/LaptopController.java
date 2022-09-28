4b489f3f9d6d4212e9be702e7c887334405aa06f
JoaquinSomoza <joaquinsomoza6@gmail.com>
2022-09-28 13:00:48 -0300

    Create en LaptopController - Ej3, sesiones 4, 5 y 6

--- src/main/java/com/example/obrestordenadores/controller/LaptopController.java
+++ src/main/java/com/example/obrestordenadores/controller/LaptopController.java
@@ -3,6 +3,8 @@ package com.example.obrestordenadores.controller;
 import com.example.obrestordenadores.entities.Laptop;
 import com.example.obrestordenadores.repository.LaptopRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,4 +25,16 @@ public class LaptopController {
     public List<Laptop> findAll(){
         return laptopRepository.findAll();
     }
+
+    @PostMapping("/api/laptops")
+    public Laptop create(@RequestBody Laptop laptop){
+        return laptopRepository.save(laptop);
+    }
 }
+
+
+// Ejercicio 3
+// Crear un método en LaptopController que reciba un objeto Laptop enviado en
+// formato JSON desde Postman y persistirlo en la base de datos.
+
+// Comprobar que al obtener de nuevo los laptops aparece el nuevo ordenador creado.
