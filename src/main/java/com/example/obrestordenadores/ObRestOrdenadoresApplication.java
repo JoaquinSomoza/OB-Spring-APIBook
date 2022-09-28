63608e52c5f7e77d6268004e3edd54ea6cf16e94
JoaquinSomoza <joaquinsomoza6@gmail.com>
2022-09-28 11:50:44 -0300

    first commit

---
+++ src/main/java/com/example/obrestordenadores/ObRestOrdenadoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.obrestordenadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObRestOrdenadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObRestOrdenadoresApplication.class, args);
+	}
+
+}
